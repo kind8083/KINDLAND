0b712566de4b77c001c82881af4d4e88884c6147
Алексей <dobrodeevaleksej42@gmail.com>
2026-08-27 19:30:42 +0400

    исправил адаптив

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ document.getElementById("randomCountry").addEventListener("click", function() {
     const randomIndex = Math.floor(Math.random() * countries.length);
     window.location.href = countries[randomIndex];
 });
+
+
+
+
+const cards = document.querySelectorAll(
+    '.geo-sea-card, .color-card, .fr-sea-card, .fr-color-card'
+);
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+
+        cards.forEach(otherCard => {
+            if (otherCard !== card) {
+                otherCard.classList.remove('active');
+            }
+        });
+
+        card.classList.toggle('active');
+    });
+});
